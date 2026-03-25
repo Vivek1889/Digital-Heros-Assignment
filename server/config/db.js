7404bf14cdd30d4dd2855c369eec0dae0334af47
vivek1889 <viveksh94108@gmail.com>
2026-03-26 04:30:10 +0530

    Setup for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2";
-
+import dotenv from "dotenv";
+dotenv.config();
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "charity",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
--- server/index.js
+++ server/index.js
@@ -38,5 +38,5 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
