ab2605cf10455ef6324ac2459beb8f97f8d36a13
vivek1889 <viveksh94108@gmail.com>
2026-03-26 05:13:25 +0530

    fix cors issue

--- server/index.js
+++ server/index.js
@@ -15,9 +15,12 @@ dotenv.config();
 const app = express();
 
 // Middlewares
+import cors from "cors";
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*", // allow all (for now)
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
